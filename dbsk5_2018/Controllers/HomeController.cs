757fc4b2a3f9f12828ef1913aafa6dade4e31cea
Marcus Brohede <a97marbr@gmail.com>
2018-10-07 00:18:00 +0200

    Seed code

---
+++ dbsk5_2018/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using dbsk5_2018.Models;
+
+namespace dbsk5_2018.Controllers
+{
+    public class HomeController : Controller
+    {
+        private StudentsModel sm = new StudentsModel("wwwlab.iki.his.se-dbsk");
+
+        public IActionResult Index()
+        {
+            ViewBag.AllStudentsTable = sm.GetAllStudents();
+            return View();
+        }
+
+        public IActionResult DeleteStudent(int id)
+        {
+            sm.DeleteStudent(id);
+            return RedirectToAction("Index");
+        }
+
+        //
+        // GET: /Home/ResetDB/
+
+        public IActionResult ResetDB()
+        {
+            sm.ResetDB();
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ dbsk5_2018/Models/StudentsModel.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Data;
+using MySql.Data.MySqlClient;
+
+namespace dbsk5_2018.Models
+{
+    public class StudentsModel
+    {
+        private string connectionString = "Server=localhost;Database=dbskdemo;User ID=ENTER_DB_USER;Password=ENTER_DB_USER_PASSWORD;Pooling=false;";
+
+        public StudentsModel(string connectionName)
+        {
+            //connectionString = System.Configuration.ConfigurationManager.ConnectionStrings[connectionName].ConnectionString;
+        }
+
+        public DataTable GetAllStudents()
+        {
+            MySqlConnection dbcon = new MySqlConnection(connectionString);
+            dbcon.Open();
+            MySqlDataAdapter adapter = new MySqlDataAdapter("SELECT * FROM Student;", dbcon);
+            DataSet ds = new DataSet();
+            adapter.Fill(ds, "result");
+            DataTable studentsTable = ds.Tables["result"];
+            dbcon.Close();
+
+            return studentsTable;
+        }
+
+        public void DeleteStudent(int id)
+        {
+            MySqlConnection dbcon = new MySqlConnection(connectionString);
+            dbcon.Open();
+            string deleteString = "DELETE FROM Student WHERE id=@ID;";
+            MySqlCommand sqlCmd = new MySqlCommand(deleteString, dbcon);
+            sqlCmd.Parameters.AddWithValue("@ID", id);
+            int rows = sqlCmd.ExecuteNonQuery();
+            dbcon.Close();
+        }
+
+        public void ResetDB()
+        {
+            MySqlConnection dbcon = new MySqlConnection(connectionString);
+            dbcon.Open();
+            string truncateTableString = "TRUNCATE TABLE Student;";
+            MySqlCommand sqlCmd = new MySqlCommand(truncateTableString, dbcon);
+            sqlCmd.ExecuteNonQuery();
+
+            string[] names = { "Kalle Karlsson", "Eva Svensson", "Olle Andersson", "Anna Larsson" };
+            string[] studyPrograms = { "DVP", "KVP", "DVP", "SVP" };
+            int[] scores = { 100, 89, 78, 98 };
+
+            for (int i = 0; (i < names.Length) && (i < studyPrograms.Length) && (i < scores.Length); i++)
+            {
+                sqlCmd = new MySqlCommand("INSERT INTO Student(name,studyProgram,score) VALUES(@NAME,@SPROGRAM,@SCORE)", dbcon);
+                sqlCmd.Parameters.AddWithValue("@NAME", names[i]);
+                sqlCmd.Parameters.AddWithValue("@SPROGRAM", studyPrograms[i]);
+                sqlCmd.Parameters.AddWithValue("@SCORE", scores[i]);
+                sqlCmd.ExecuteNonQuery();
+            }
+            dbcon.Close();
+        }
+    }
+}
